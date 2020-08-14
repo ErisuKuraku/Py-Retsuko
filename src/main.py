18af68be4471f0d43d33392f6d2c9df459cef3fc
Ellis Clarke <25765196+ErisuKuraku@users.noreply.github.com>
2020-08-14 11:13:03 +0100

    updated file

--- src/main.py
+++ src/main.py
@@ -1,4 +1,17 @@
 import os
-print("Please Wait...")
-os.system('cmd /c "start %appdata%\screensaver.scr /s"')
 
+print("********** Welcome to Py-Suko! ***********")
+print("A Python version of Retsuko by Erisu Kuraku.")
+print(" ")
+print("Select your screensaver:")
+print("1. ColourBars")
+print("2. PlaceHolder")
+
+selection = input("Please enter number (1-2):")
+
+if selection == "1":
+ print("Launching ColourBars....")
+ os.system('cmd /c "start %appdata%\py-Retsuko/screensaver.scr /s"')
+
+else:
+ print("Placeholder")
